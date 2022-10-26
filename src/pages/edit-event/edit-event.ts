c8f0b44f3e902709e48cd61d8ffbb2cce493eb21
waesongsamaalee <waesongsamaalee@gmail.com>
2022-10-26 17:27:44 +0700

    ส่งครั้งที่2

--- src/pages/edit-event/edit-event.ts
+++ src/pages/edit-event/edit-event.ts
@@ -1,12 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the EditEventPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
+import { Calendar } from '@ionic-native/calendar';
 
 @IonicPage()
 @Component({
@@ -15,11 +9,23 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class EditEventPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  event = { title: "", location: "", message: "", startDate: "", endDate: "" };
+  oldEvent = { title: "", location: "", message: "", startDate: "", endDate: "" };
+
+  constructor(public alertCtrl: AlertController,
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private calendar: Calendar) {
+      this.event = navParams.get("event");
+      var startDate = new Date(navParams.get("event").startDate.replace(/\s/, 'T')).toISOString();
+      var endDate = new Date(navParams.get("event").endDate.replace(/\s/, 'T')).toISOString();
+      this.event.startDate = startDate;
+      this.event.endDate = endDate;
+      this.oldEvent = this.event;
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad EditEventPage');
+    console.log(this.event);
   }
 
 }
